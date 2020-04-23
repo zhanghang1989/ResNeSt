1587467d89716a0b3438a1561752becc3b1396ca
DevinYang <pistonyang@gmail.com>
2020-04-23 11:53:07 +0800

    remove dirt changes.

--- resnest/torch/splat.py
+++ resnest/torch/splat.py
@@ -8,11 +8,9 @@ from torch.nn.modules.utils import _pair
 
 __all__ = ['SplAtConv2d']
 
-
 class SplAtConv2d(Module):
     """Split-Attention Conv2d
     """
-
     def __init__(self, in_channels, channels, kernel_size, stride=(1, 1), padding=(0, 0),
                  dilation=(1, 1), groups=1, bias=True,
                  radix=2, reduction_factor=4,
@@ -22,26 +20,26 @@ class SplAtConv2d(Module):
         padding = _pair(padding)
         self.rectify = rectify and (padding[0] > 0 or padding[1] > 0)
         self.rectify_avg = rectify_avg
-        inter_channels = max(in_channels * radix // reduction_factor, 32)
+        inter_channels = max(in_channels*radix//reduction_factor, 32)
         self.radix = radix
         self.cardinality = groups
         self.channels = channels
         self.dropblock_prob = dropblock_prob
         if self.rectify:
             from rfconv import RFConv2d
-            self.conv = RFConv2d(in_channels, channels * radix, kernel_size, stride, padding, dilation,
-                                 groups=groups * radix, bias=bias, average_mode=rectify_avg, **kwargs)
+            self.conv = RFConv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
+                                 groups=groups*radix, bias=bias, average_mode=rectify_avg, **kwargs)
         else:
-            self.conv = Conv2d(in_channels, channels * radix, kernel_size, stride, padding, dilation,
-                               groups=groups * radix, bias=bias, **kwargs)
+            self.conv = Conv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
+                               groups=groups*radix, bias=bias, **kwargs)
         self.use_bn = norm_layer is not None
         if self.use_bn:
-            self.bn0 = norm_layer(channels * radix)
+            self.bn0 = norm_layer(channels*radix)
         self.relu = ReLU(inplace=True)
         self.fc1 = Conv2d(channels, inter_channels, 1, groups=self.cardinality)
         if self.use_bn:
             self.bn1 = norm_layer(inter_channels)
-        self.fc2 = Conv2d(inter_channels, channels * radix, 1, groups=self.cardinality)
+        self.fc2 = Conv2d(inter_channels, channels*radix, 1, groups=self.cardinality)
         if dropblock_prob > 0.0:
             self.dropblock = DropBlock2D(dropblock_prob, 3)
         self.rsoftmax = rSoftMax(radix, groups)
