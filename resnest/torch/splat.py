59bbb4a27d5ac6cd679aa5b36389ae7277c731d7
DevinYang <pistonyang@gmail.com>
2020-04-23 11:42:46 +0800

    improve implement to avoid for-loop in forward.

--- resnest/torch/splat.py
+++ resnest/torch/splat.py
@@ -8,9 +8,11 @@ from torch.nn.modules.utils import _pair
 
 __all__ = ['SplAtConv2d']
 
+
 class SplAtConv2d(Module):
     """Split-Attention Conv2d
     """
+
     def __init__(self, in_channels, channels, kernel_size, stride=(1, 1), padding=(0, 0),
                  dilation=(1, 1), groups=1, bias=True,
                  radix=2, reduction_factor=4,
@@ -20,26 +22,26 @@ class SplAtConv2d(Module):
         padding = _pair(padding)
         self.rectify = rectify and (padding[0] > 0 or padding[1] > 0)
         self.rectify_avg = rectify_avg
-        inter_channels = max(in_channels*radix//reduction_factor, 32)
+        inter_channels = max(in_channels * radix // reduction_factor, 32)
         self.radix = radix
         self.cardinality = groups
         self.channels = channels
         self.dropblock_prob = dropblock_prob
         if self.rectify:
             from rfconv import RFConv2d
-            self.conv = RFConv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
-                                 groups=groups*radix, bias=bias, average_mode=rectify_avg, **kwargs)
+            self.conv = RFConv2d(in_channels, channels * radix, kernel_size, stride, padding, dilation,
+                                 groups=groups * radix, bias=bias, average_mode=rectify_avg, **kwargs)
         else:
-            self.conv = Conv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
-                               groups=groups*radix, bias=bias, **kwargs)
+            self.conv = Conv2d(in_channels, channels * radix, kernel_size, stride, padding, dilation,
+                               groups=groups * radix, bias=bias, **kwargs)
         self.use_bn = norm_layer is not None
         if self.use_bn:
-            self.bn0 = norm_layer(channels*radix)
+            self.bn0 = norm_layer(channels * radix)
         self.relu = ReLU(inplace=True)
         self.fc1 = Conv2d(channels, inter_channels, 1, groups=self.cardinality)
         if self.use_bn:
             self.bn1 = norm_layer(inter_channels)
-        self.fc2 = Conv2d(inter_channels, channels*radix, 1, groups=self.cardinality)
+        self.fc2 = Conv2d(inter_channels, channels * radix, 1, groups=self.cardinality)
         if dropblock_prob > 0.0:
             self.dropblock = DropBlock2D(dropblock_prob, 3)
         self.rsoftmax = rSoftMax(radix, groups)
@@ -52,10 +54,10 @@ class SplAtConv2d(Module):
             x = self.dropblock(x)
         x = self.relu(x)
 
-        batch, rchannel = x.shape[:2]
+        batch, rchannel, h, w = x.size()
         if self.radix > 1:
-            splited = torch.split(x, rchannel//self.radix, dim=1)
-            gap = sum(splited) 
+            splited = torch.reshape(x, (batch, self.radix, self.channels, h, w))
+            gap = torch.sum(splited, dim=1)
         else:
             gap = x
         gap = F.adaptive_avg_pool2d(gap, 1)
@@ -65,29 +67,23 @@ class SplAtConv2d(Module):
             gap = self.bn1(gap)
         gap = self.relu(gap)
 
-        atten = self.fc2(gap)
-        atten = self.rsoftmax(atten).view(batch, -1, 1, 1)
+        atten = self.fc2(gap).reshape((batch, self.radix, self.channels, 1, 1))
+        atten = self.rsoftmax(atten)
 
         if self.radix > 1:
-            attens = torch.split(atten, rchannel//self.radix, dim=1)
-            out = sum([att*split for (att, split) in zip(attens, splited)])
+            out = torch.sum(splited * atten, dim=1)
         else:
             out = atten * x
-        return out.contiguous()
+        return out
+
 
 class rSoftMax(nn.Module):
-    def __init__(self, radix, cardinality):
+    def __init__(self):
         super().__init__()
-        self.radix = radix
-        self.cardinality = cardinality
 
     def forward(self, x):
-        batch = x.size(0)
         if self.radix > 1:
-            x = x.view(batch, self.cardinality, self.radix, -1).transpose(1, 2)
-            x = F.softmax(x, dim=1)
-            x = x.reshape(batch, -1)
+            x = torch.softmax(x, dim=1)
         else:
             x = torch.sigmoid(x)
         return x
-
