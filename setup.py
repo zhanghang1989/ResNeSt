d00003fc2a865229b0cd8332bb415fa296f7db31
Hang Zhang <zhang.hang@rutgers.edu>
2020-06-21 14:26:19 -0700

    bump up version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-version = '0.0.4'
+version = '0.0.5'
 try:
     if not os.getenv('RELEASE'):
         from datetime import date
