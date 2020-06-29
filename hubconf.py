5fe47e93bd7e098d15bc278d8ab4812b82b49414
Hang Zhang <zhang.hang@rutgers.edu>
2020-06-29 09:31:13 -0700

    Update hubconf.py (#93)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,6 @@
 dependencies = ['torch']
 
 from resnest.torch.resnest import resnest50, resnest101, resnest200, resnest269
+from resnest.torch.ablation import (resnest50_fast_1s1x64d, resnest50_fast_2s1x64d, resnest50_fast_4s1x64d,
+                                    resnest50_fast_1s2x40d, resnest50_fast_2s2x40d, resnest50_fast_4s2x40d,
+                                    resnest50_fast_1s4x24d)
